dbebe6b596d10eff1b6cdb32e35441e9ed3927a0
Le-Yo <lenykoskey@gmail.com>
2020-05-09 13:36:38 +0300

    fix www-data

--- recipe/cachedir.php
+++ recipe/cachedir.php
@@ -4,5 +4,5 @@ namespace Deployer;
 
 desc('Create cache dir');
 task('create_cache_dir', function () {
-    run("cd {{release_path}}/public && mkdir page-cache && chmod -R 775 page-cache && chown -R wwww-data:www-data page-cache ");
+    run("cd {{release_path}}/public && mkdir page-cache && chmod -R 775 page-cache && chown -R www-data:www-data page-cache ");
 });
